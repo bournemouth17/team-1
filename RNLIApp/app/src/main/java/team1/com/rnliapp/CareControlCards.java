4aff2fd837c3632a167fc5a3c3ed8aa9c4e78595
albylillington <albylillington@gmail.com>
2017-06-10 00:27:43 +0100

    Added activity for illness classes:

--- RNLIApp/app/src/main/java/team1/com/rnliapp/CareControlCards.java
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/CareControlCards.java
@@ -2,6 +2,8 @@ package team1.com.rnliapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CareControlCards extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class CareControlCards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_care_control_cards);
+        this.setListeners();
+
+    }
+
+    protected void setListeners(){
+        Button card1 = (Button)(findViewById(R.id.card1));
+        Button card2 = (Button)(findViewById(R.id.card2));
+        Button card3 = (Button)(findViewById(R.id.card3));
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open card 1
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open card 2
+
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open card 3
+            }
+        });
     }
 }

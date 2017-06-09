62ac9f517b706b0ece456a8a1656f29e949b852c
Harvey <harveyives@gmail.com>
2017-06-09 20:26:42 +0100

    harvey's commit test

--- RNLIApp/app/src/main/java/team1/com/rnliapp/MainActivity.java
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //testing that this comment is committing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

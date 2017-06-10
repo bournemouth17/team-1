f6a1e2b5c8afb397c8deacaf02c1ac2e18fed258
NickDenman <1998.denman@gmail.com>
2017-06-10 04:30:00 +0100

    My fourth commit
    Timer finally works for breath count

--- RNLIApp/app/src/main/java/team1/com/rnliapp/InitialPatientCheck.java
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/InitialPatientCheck.java
@@ -1,17 +1,24 @@
 package team1.com.rnliapp;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class InitialPatientCheck extends AppCompatActivity {
     EditText questionBox;
     ImageButton yesButton;
     ImageButton noButton;
+    Button startButton;
+    Button submitButton;
 
     int count = 0;
     PredictIssue issue = new PredictIssue();
@@ -23,21 +30,26 @@ public class InitialPatientCheck extends AppCompatActivity {
                           "Is the patient in pain?",
                           "Is the patient responsive?",
                           "Does the patient have any life threatening bleeding?",
-                          "Check the patient's breathing rate for 10 seconds and x 6. Consider depth and quality.",
-                          "Check the patient's central capillary refill - heart/fluid loss?",
                           "Is the patient slumped/tripod/lying down?",
-                          "Is the patient conscious?"};
+                          "Is the patient conscious?",
+                          "Check the patient's breathing rate for 10 seconds. Consider depth and quality.",
+                          "Check the patient's central capillary refill - heart/fluid loss?"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_patient_check);
-        questionBox = (EditText) (findViewById(R.id.editText));
+
+                questionBox = (EditText) (findViewById(R.id.editText));
         questionBox.setText(questions[0]);
 
 
         yesButton = (ImageButton)(findViewById(R.id.imageButton3));
         noButton = (ImageButton)(findViewById(R.id.imageButton2));
+        startButton = (Button)(findViewById(R.id.startButton));
+        submitButton = (Button)(findViewById((R.id.submitButton)));
+        startButton.setVisibility(View.INVISIBLE);
+        submitButton.setVisibility(View.INVISIBLE);
 
         yesButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -53,6 +65,38 @@ public class InitialPatientCheck extends AppCompatActivity {
                 System.out.println("NO BUTTON");
             }
         });
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                new CountDownTimer(10000, 50) {
+
+                    public void onTick(long millisUntilFinished) {
+                        questionBox.setText("" + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        questionBox.setText("Timer done. Enter breath count here");
+                        startButton.setVisibility(View.INVISIBLE);
+                        submitButton.setVisibility(View.VISIBLE);
+                    }
+                }.start();
+            }
+        });
+
+        questionBox.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                questionBox.setText("");
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                buttonClick(Integer.parseInt(questionBox.getText().toString())*6);
+                submitButton.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
     private void buttonClick(int result){
@@ -65,18 +109,8 @@ public class InitialPatientCheck extends AppCompatActivity {
         else count++;
 
         questionBox.setText(questions[count]);
-        if(count == 8){
-            breathTimer();
+        if(count == 10){
+            startButton.setVisibility(View.VISIBLE);
         }
-    }
-
-    private void breathTimer(){
-        ViewGroup yesLayout = (ViewGroup) yesButton.getParent();
-        ViewGroup noLayout = (ViewGroup) noButton.getParent();
-        if(null!=yesLayout && null!=noLayout){
-            yesLayout.removeView(yesButton);
-            noLayout.removeView(noButton);
-        }
-
     }
 }

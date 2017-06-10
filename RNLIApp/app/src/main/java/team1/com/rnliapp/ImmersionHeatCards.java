f27a3ebbae78331a5d95c31c7ff48ae2dc3f65e4
albylillington <albylillington@gmail.com>
2017-06-10 01:08:31 +0100

    Completed most of the card buttons

---
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/ImmersionHeatCards.java
@@ -0,0 +1,13 @@
+package team1.com.rnliapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImmersionHeatCards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immersion_heat_cards);
+    }
+}

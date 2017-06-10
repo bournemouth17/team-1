95835256eedf2a3b1af6969e8697272e093e5705
albylillington <albylillington@gmail.com>
2017-06-10 01:17:38 +0100

    Finished all card buttons

--- RNLIApp/app/src/main/java/team1/com/rnliapp/ImmersionHeatCards.java
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/ImmersionHeatCards.java
@@ -2,6 +2,8 @@ package team1.com.rnliapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ImmersionHeatCards extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class ImmersionHeatCards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_immersion_heat_cards);
+        this.setListeners();
+    }
+
+    protected void setListeners(){
+        Button card17 = (Button)(findViewById(R.id.card17));
+        Button card18 = (Button)(findViewById(R.id.card18));
+        Button card19 = (Button)(findViewById(R.id.card19));
+
+        card17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open card 17
+            }
+        });
+
+        card18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open card 18
+
+            }
+        });
+
+        card19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open card 19
+            }
+        });
     }
 }

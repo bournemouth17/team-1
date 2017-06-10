0779de0e065d2f8d5280aad1bfc32b17bb02e787
albylillington <albylillington@gmail.com>
2017-06-10 05:55:10 +0100

    Made patient notification activity every x seconds

--- RNLIApp/app/src/main/java/team1/com/rnliapp/MainActivity.java
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/MainActivity.java
@@ -6,29 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Button startButton = (Button)findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchMainNavigationActivity();
+                Timer t = new Timer();
+                t.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        timerActivity();
+                    }
+                }, 3000, 3000);
             }
         });
 
-        Button b = (Button)(findViewById(R.id.button2));
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, PatientInfo.class);
-                startActivity(i);
-            }
-        });
 
+    }
+
+    public void timerActivity(){
+        //start redo assesment activity
+        //TO TEST, DELETE AFTER
+        Intent in = new Intent(MainActivity.this, ReassessNotifActivity.class);
+        startActivity(in);
     }
 
     private void launchMainNavigationActivity() {

1fdfbe3f692fb07bb399d104ca08f5af29f891e3
Alex Eales <ae470@bath.ac.uk>
2017-06-10 02:32:05 +0100

    Card Number Pass Test

--- RNLIApp/app/src/main/java/team1/com/rnliapp/InformationCardDisplay.java
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/InformationCardDisplay.java
@@ -2,6 +2,8 @@ package team1.com.rnliapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InformationCardDisplay extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class InformationCardDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_card_display);
+        int cardNumber = getIntent().getIntExtra("CARD_NUMBER", 0);
+        TextView text = (TextView) findViewById(R.id.cardNumberView);
+        text.setText("Card Number: " + cardNumber);
     }
+
 }

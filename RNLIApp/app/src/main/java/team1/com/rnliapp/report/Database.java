a8ab627dd16614bd960bcb5624185894324db75e
Harvey <harveyives@gmail.com>
2017-06-10 01:08:48 +0100

    initial database commit

---
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/report/Database.java
@@ -0,0 +1,60 @@
+package team1.com.rnliapp.report;
+
+import java.sql.*;
+/**
+ * Created by harvey on 10/06/2017.
+ */
+
+public class Database {
+    private static Connection connection = null;
+    private static Statement statement = null;
+
+    public Database(String url, String user, String pass){
+        System.out.println("-------- MySQL JDBC Connection Testing ------------");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC DRIVER ERROR");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("DATABASE REGISTERED");
+        try {
+            connection = DriverManager
+                    .getConnection(url, user, pass);
+
+        } catch (SQLException e) {
+            System.out.println("CONNECTION FAILED");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("CONNECTED TO DATABASE");
+        } else {
+            System.out.println("CONNECTION FAILED");
+        }
+    }
+    public static ResultSet executeQuery(String sql){
+        try{
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            System.out.println("QUERY EXECUTED");
+            return rs;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void executeUpdate(String sql){
+        try{
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            System.out.println("QUERY EXECUTED");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}

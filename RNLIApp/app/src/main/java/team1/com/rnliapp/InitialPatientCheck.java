61e4afacd7bd081dda37251a4af0c6249776fd28
NickDenman <1998.denman@gmail.com>
2017-06-10 01:36:38 +0100

    My third commit
    Filenames of drawable images

--- RNLIApp/app/src/main/java/team1/com/rnliapp/InitialPatientCheck.java
+++ RNLIApp/app/src/main/java/team1/com/rnliapp/InitialPatientCheck.java
@@ -3,12 +3,16 @@ package team1.com.rnliapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class InitialPatientCheck extends AppCompatActivity {
     EditText questionBox;
+    ImageButton yesButton;
+    ImageButton noButton;
+
     int count = 0;
     PredictIssue issue = new PredictIssue();
     String[] questions = {"Is the patient injured?",
@@ -32,8 +36,8 @@ public class InitialPatientCheck extends AppCompatActivity {
         questionBox.setText(questions[0]);
 
 
-        ImageButton yesButton = (ImageButton)(findViewById(R.id.imageButton3));
-        ImageButton noButton = (ImageButton)(findViewById(R.id.imageButton2));
+        yesButton = (ImageButton)(findViewById(R.id.imageButton3));
+        noButton = (ImageButton)(findViewById(R.id.imageButton2));
 
         yesButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -61,5 +65,18 @@ public class InitialPatientCheck extends AppCompatActivity {
         else count++;
 
         questionBox.setText(questions[count]);
+        if(count == 8){
+            breathTimer();
+        }
+    }
+
+    private void breathTimer(){
+        ViewGroup yesLayout = (ViewGroup) yesButton.getParent();
+        ViewGroup noLayout = (ViewGroup) noButton.getParent();
+        if(null!=yesLayout && null!=noLayout){
+            yesLayout.removeView(yesButton);
+            noLayout.removeView(noButton);
+        }
+
     }
 }
